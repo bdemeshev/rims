72970ef19b4a52ba69d7476e020520201177df3c
Boris Demeshev <boris.demeshev@gmail.com>
2015-06-23 15:36:17 +0300

    forgotten {} in str_utf2cp()

--- R/rims.R
+++ R/rims.R
@@ -228,9 +228,10 @@ str_utf2cp <- function(x,backward=FALSE) {
   ans <- x
   if (is.character(x) | is.factor(x))
     ans <- str_utf2cp0(x, backward = backward)
-  if (is.data.frame(x)) 
+  if (is.data.frame(x)) {
     colnames(ans) <- str_utf2cp0(colnames(ans), backward = backward)
     for (i in 1:ncol(ans)) ans[,i] <- str_utf2cp0(ans[,i], backward = backward)        
+  }
   return(ans)
 }
 
